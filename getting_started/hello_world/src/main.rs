3e6486847e4413ed8a15b490b3993e0fe2c5cc5f
Carl Edwards <ecarl42@gmail.com>
2020-04-04 13:39:45 -0700

    inital rust async "getting_started" examples

---
+++ getting_started/hello_world/src/main.rs
@@ -0,0 +1,13 @@
+
+extern crate futures;
+
+use futures::executor::block_on;
+
+async fn hello_world() {
+    println!("hello, world!");
+}
+
+fn main() {
+    let future = hello_world();
+    block_on(future);
+}
